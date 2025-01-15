951e92b9237de9ae8cf4ce59d52c00e127beb4ab
williammokonemafube@gmail.com <williammokonemafube@gmail.com>
2025-01-15 15:39:50 +0200

    hello touch lessss goooooo

--- Untitled-1.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT
- MIN(record_date) AS begin_2021, MAX(record_date) as closing_date_2021
-FROM 
-transactiondata
-where record_date BETWEEN '2021-07-01 00:00:00' AND '2021-12-31 00:00:00'
-;
