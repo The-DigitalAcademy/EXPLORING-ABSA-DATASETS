c707b2deaa3cd24776a010ce81199fd57fce6d36
williammokonemafube@gmail.com <williammokonemafube@gmail.com>
2025-01-15 15:38:09 +0200

    hi

--- --which date we had less transaction rec.sql
+++
@@ -1,6 +0,0 @@
---which date we had less transaction recorded
-SELECT RECORD_DATE,COUNT(*) AS less_transaction_count
-FROM transactiondata
-GROUP BY record_date
-ORDER BY less_transaction_count
-LIMIT 10;
